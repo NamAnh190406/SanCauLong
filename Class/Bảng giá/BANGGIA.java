ff175ba9e527e2f1b85a2d728202fa6a369aabfa
NamAnh190406 <24520135@gm.uit.edu.vn>
2026-05-08 11:52:44 +0700

    BANGGIA.java
    Signed-off-by: NamAnh190406 <24520135@gm.uit.edu.vn>

---
+++ Class/Bảng giá/BANGGIA.java
@@ -0,0 +1,17 @@
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.Date;
+
+public class BANGGIA {
+    public String MaBG;
+    public long DonGia;
+    public String MaSan;
+    public String MaKG;
+    public Connection conn;
+
+    public BANGGIA(Connection conn) {
+        this.conn = conn;
+    }
+
+    // Khong co ham nao khac lien quan:(
+}
