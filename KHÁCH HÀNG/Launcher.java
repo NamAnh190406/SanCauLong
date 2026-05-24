a9df4249559bf42f330ca9f5136e3899b1d34a88
NamAnh190406 <24520135@gm.uit.edu.vn>
2026-05-24 12:57:22 +0700

    Delete KHÁCH HÀNG directory
    Signed-off-by: NamAnh190406 <24520135@gm.uit.edu.vn>

--- KHÁCH HÀNG/Launcher.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.mavenproject1;
-
-public class Launcher {
-    public static void main(String[] args) {
-        // Gọi hàm main của App từ đây sẽ bypass được lỗi "missing runtime components"
-        App.main(args); 
-    }
-}
--- KHÁCH HÀNG/LuuThongTinDangNhap.java
+++
@@ -1,8 +0,0 @@
-package com.mycompany.mavenproject1;
-
-public class LuuThongTinDangNhap {
-    public static String maKH;
-    public static String hoTen;
-    public static int diemTichLuy;
-    public static String hangThanhVien;
-}
